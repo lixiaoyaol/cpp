990ac4dfaf98c713503c3b23e1ec1911d7e20ed9
libofeng <bf_li@qq.com>
2024-07-11 12:42:54 +0800

    Update

--- matrix_main.cpp
+++ matrix_main.cpp
@@ -14,25 +14,27 @@ int main()
 
     vector<float> data = {1, 2, 3, 4, 5, 6};
     Matrix mat1(2, 3, data);
-    vector<float> data2 = {7, 8, 9, 10, 11, 12};
-    Matrix mat2(2, 3, data2);
-    Matrix mat3 = mat1 + mat2;
-    Matrix mat4 = mat1 - mat2;
-    Matrix mat5 = mat1 * 2;
-    Matrix mat10 = 2 * mat1;
-    Matrix mat6 = mat1 * mat2;
+    // vector<float> data2 = {7, 8, 9, 10, 11, 12};
+    // Matrix mat2(2, 3, data2);
+    // Matrix mat3 = mat1 + mat2;
+    // Matrix mat4 = mat1 - mat2;
+    // Matrix mat5 = mat1 * 2;
+    // Matrix mat10 = 2 * mat1;
+    // Matrix mat6 = mat1 * mat2;
 
-    Matrix mat7(2, 3, data);
-    Matrix mat8(3, 2, data2);
+    // Matrix mat7(2, 3, data);
+    // Matrix mat8(3, 2, data2);
 
-    Matrix mat9 = matmul(mat7, mat8);
+    // Matrix mat9 = matmul(mat7, mat8);
 
-    cout << mat9 << endl;
-    cout << mat9.transpose() << endl;
-    cout << mat1 << endl;
-    cout << mat1.transpose() << endl;
-    cout << mat2 << endl;
-    cout << mat2.transpose() << endl;
+    // cout << mat9 << endl;
+    // cout << mat9.transpose() << endl;
+    // cout << mat1 << endl;
+    // cout << mat1.transpose() << endl;
+    // cout << mat2 << endl;
+    // cout << mat2.transpose() << endl;
+    Matrix mat11 = mat1;
+    cout << mat11 << endl;
 
     return 0;
 }
